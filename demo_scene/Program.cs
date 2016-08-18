6ee9fc7d657a227b67d5083f8677b1ff63892aeb
Carsten Wollsen <carsten.wollsen@gmail.com>
2016-08-18 16:06:59 +0200

    do while

--- demo_scene/Program.cs
+++ demo_scene/Program.cs
@@ -10,6 +10,39 @@ namespace demo_scene
 	{
 		static void Main(string[] args)
 		{
+			int x;
+			int y;
+			var rand = new Random();
+
+			do
+			{
+				while (!Console.KeyAvailable)
+				{
+					x = rand.Next(79) + 1;
+					y = rand.Next(23) + 1;
+
+					if ((rand.Next(2) + 1)%2 == 0)
+					{
+						Console.ForegroundColor = ConsoleColor.DarkGreen;
+						//Console.Clear();
+					}
+					else
+					{
+						Console.ForegroundColor = ConsoleColor.Green;
+						//Console.Clear();
+					}
+					Console.SetCursorPosition(x, y);
+
+					if ((rand.Next(2) + 1)%2 == 0)
+					{
+						Console.Write("1");
+					}
+					else
+					{
+						Console.Write("0");
+					}
+				}
+			} while (Console.ReadKey(true).Key != ConsoleKey.Enter);
 		}
 	}
 }
