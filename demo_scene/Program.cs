75e142e3da44e2507cd6a2563a33fc09ed34b3b3
Carsten Wollsen <carsten.wollsen@gmail.com>
2016-08-23 14:31:42 +0200

    comparing points

--- demo_scene/Program.cs
+++ demo_scene/Program.cs
@@ -1,25 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using demo_scene.helpers;
 
 namespace demo_scene
 {
 	class Program
 	{
+		private static HashSet<ConsolePoint> d;
+
 		static void Main(string[] args)
 		{
 			int x;
 			int y;
+			ConsolePoint a;
 			var rand = new Random();
 
+
+
+			var comparer = new ConsolePointComparer<ConsolePoint>();
+			var usedCoords = new HashSet<ConsolePoint>(comparer);
+			d = new HashSet<ConsolePoint>(comparer);
+			CreateD();
+			var testPoint = new ConsolePoint(16, 1);
+
+			var contains = d.Contains(testPoint);
+			if (contains)
+			{
+				usedCoords.Add(testPoint);
+			}
+
+			Console.SetCursorPosition(testPoint.X, testPoint.Y);
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.Write('W');
 			do
+
 			{
 				while (!Console.KeyAvailable)
 				{
-					x = rand.Next(79) + 1;
-					y = rand.Next(23) + 1;
+					do
+					{
+						x = rand.Next(79) + 1;
+						y = rand.Next(23) + 1;
+						a = new ConsolePoint(x, y);
+					} while (usedCoords.Contains(a));
+
+					
+					
+
 					var random = rand.Next(3) + 1;
 					if (random % 3 == 0)
 					{
@@ -33,7 +64,7 @@ namespace demo_scene
 					{
 						Console.ForegroundColor = ConsoleColor.DarkYellow;
 					}
-					Console.SetCursorPosition(x, y);
+					Console.SetCursorPosition(a.X, a.Y);
 
 					var key = rand.Next(32, 90);
 
@@ -46,7 +77,26 @@ namespace demo_scene
 						Console.Write("0");
 					}
 				}
-			} while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+			}
+			while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+		}
+
+		private static void CreateD()
+		{
+			d.Add(new ConsolePoint(16, 1));
+		}
+	}
+
+	public class ConsolePointComparer<T> : IEqualityComparer<ConsolePoint>
+	{
+		public bool Equals(ConsolePoint x, ConsolePoint y)
+		{
+			return x.X == y.X && x.Y == y.Y;
+		}
+
+		public int GetHashCode(ConsolePoint obj)
+		{
+			return obj.X*17 + obj.Y*31;
 		}
 	}
 }
---
+++ demo_scene/helpers/ConsolePoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_scene.helpers
+{
+	public class ConsolePoint
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public ConsolePoint(int x, int y)
+		{
+			this.X = x;
+			this.Y = y;
+		}
+	}
+}
